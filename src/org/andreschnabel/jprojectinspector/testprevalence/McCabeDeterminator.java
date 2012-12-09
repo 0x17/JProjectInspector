3a4dc425f9e3fa7e64e0f6d9a72d37cc4b926b78
0x17 <andreschnabel@me.com>
2012-12-09 07:05:02 +0100

    Added mccabe stub

---
+++ src/org/andreschnabel/jprojectinspector/testprevalence/McCabeDeterminator.java
@@ -0,0 +1,30 @@
+package org.andreschnabel.jprojectinspector.testprevalence;
+
+import org.andreschnabel.jprojectinspector.Helpers;
+
+import java.io.File;
+
+public class McCabeDeterminator {
+
+	public int determineMcCabeForSrcStr(String srcStr) {
+		return Helpers.countOccurencesOfWords(srcStr, new String[]{"switch", "if", "else", "case"});
+	}
+
+	public float determineMcCabeForSrcFile(File srcFile) throws Exception {
+		return determineMcCabeForSrcStr(Helpers.readEntireFile(srcFile));
+	}
+
+	public float determineMcCabeForDir(File root) throws Exception {
+		if(root.isDirectory()) {
+			int sum = 0;
+			int count = root.listFiles().length;
+			for(File f : root.listFiles()) {
+				sum += determineMcCabeForDir(f);
+			}
+			return (float)sum / count;
+		}
+		else
+			return determineMcCabeForSrcFile(root);
+	}
+
+}
