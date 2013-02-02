8f97812bcfe66d31592df77a72d59ffdcb9cc1b9
0x17 <andreschnabel@me.com>
2013-02-02 07:22:13 +0100

    Changed device letter for Windows used for Temp folder.

--- src/org/andreschnabel/jprojectinspector/Config.java
+++ src/org/andreschnabel/jprojectinspector/Config.java
@@ -4,7 +4,7 @@ import org.andreschnabel.jprojectinspector.utilities.helpers.Helpers;
 
 public final class Config {	
 	public final static String DEST_BASE_UNIX = "/tmp/";
-	public final static String DEST_BASE_WIN = "E:\\Temp\\";
+	public final static String DEST_BASE_WIN = "D:\\Temp\\";
 	public final static String DEST_BASE = Helpers.runningOnUnix() ? DEST_BASE_UNIX : DEST_BASE_WIN;
 	public final static String BASE_URL = "https://github.com/";
 }
