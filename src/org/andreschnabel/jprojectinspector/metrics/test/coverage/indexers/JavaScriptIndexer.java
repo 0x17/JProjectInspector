b1dfe2bbc211119cfe351285dec0005f1a46b194
0x17 <andreschnabel@me.com>
2013-04-13 00:25:01 +0200

    JavaScript indexer and corresponding test.

--- src/org/andreschnabel/jprojectinspector/metrics/test/coverage/indexers/JavaScriptIndexer.java
+++ src/org/andreschnabel/jprojectinspector/metrics/test/coverage/indexers/JavaScriptIndexer.java
@@ -1,18 +1,40 @@
 package org.andreschnabel.jprojectinspector.metrics.test.coverage.indexers;
 
 import org.andreschnabel.jprojectinspector.metrics.test.coverage.FunctionIndexer;
+import org.andreschnabel.jprojectinspector.utilities.Predicate;
+import org.andreschnabel.jprojectinspector.utilities.helpers.ListHelpers;
+import org.andreschnabel.jprojectinspector.utilities.helpers.RegexHelpers;
 
-import java.util.LinkedList;
 import java.util.List;
 
 public class JavaScriptIndexer implements FunctionIndexer {
 	@Override
 	public List<String> listFunctionDeclarations(String src) {
-		return new LinkedList<String>();
+		List<String> funcIdentifiers = null;
+		try {
+			funcIdentifiers = RegexHelpers.batchMatchOneGroup("function (\\w+)\\(", src);
+			funcIdentifiers.addAll(RegexHelpers.batchMatchOneGroup("(\\w+) = function\\(\\)", src));
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return ListHelpers.remDups(funcIdentifiers);
 	}
 
 	@Override
-	public List<String> listFunctionCalls(String src) {
-		return new LinkedList<String>();
+	public List<String> listFunctionCalls(final String src) {
+		final List<String> declaredFuncs = listFunctionDeclarations(src);
+		List<String> funcIdentifiers = null;
+		try {
+			funcIdentifiers = RegexHelpers.batchMatchOneGroup("([A-Za-z]\\w*)[\\(\\)]", src);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		Predicate<String> notDeclaredAndNotConstructor = new Predicate<String>() {
+			@Override
+			public boolean invoke(String s) {
+				return !declaredFuncs.contains(s) && !src.contains("new " + s);
+			}
+		};
+		return ListHelpers.remDups(ListHelpers.filter(notDeclaredAndNotConstructor, funcIdentifiers));
 	}
 }
